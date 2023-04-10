5b9dee15f1bf66c6b686c5dba2c2911ca96354e2
TeddyBear7777 <50842237+TeddyBear7777@users.noreply.github.com>
2023-04-10 17:11:06 +0300

    Код для предсказаний
    Код для загрузки нейронки из файла и предсказания

---
+++ predict.py
@@ -0,0 +1,19 @@
+import keras
+import numpy as np
+from keras.utils import load_img, img_to_array
+
+
+model = keras.models.load_model('neuro.h5')
+
+img_path = 'data/test/overheating/ov61.jpg'   # input image path
+img = load_img(img_path, target_size=(80, 80))
+classes = [bin(i).replace("0b", "") for i in range(128)]
+
+x = img_to_array(img)
+x = 255 - x
+x /= 255
+x = np.expand_dims(x, axis=0)
+
+prediction = model.predict(x)
+prediction = np.argmax(prediction, axis=1)
+print(classes[prediction[0]])   # вывод предсказания
